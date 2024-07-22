9f6dc6f99e577077138d9e2966aa34881964842f
Chigoziem Edmund Ofili <goziem.ofili@gmail.com>
2024-07-21 23:15:03 -0600

    Updated YouTube Link

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Head from "next/head";
 import type { NextPage } from "next";
-import VencruTest from "@/img/Unknown-4.png"
-import Dashboard from "@/img/dashboard.png"
-import Travel from "@/img/traveler.png"
+import VencruTest from "@/img/Unknown-4.png";
+import Dashboard from "@/img/dashboard.png";
+import Travel from "@/img/traveler.png";
 
 import PrimaryLayout from "@/components/template/PrimaryLayout";
 import Link from "next/link";
@@ -27,12 +27,14 @@ const Projects: NextPage = () => {
               <div className="grid grid-cols-2 grid-rows-2 gap-8 w-[100%] h-[100%] p-8">
                 <section className="flex flex-col items-center w-full h-full gap-4">
                   <div className="rounded-md overflow-hidden">
+                    <Link href="https://www.youtube.com/watch?v=JOEfLlxs9mY" className="">
                       <Image src={Dashboard} alt="test"/>
+                    </Link>
                   </div>
                   <div className="font-extrabold text-violet-400 text-center"> 
-                    {/* <Link href="https://vencru-test-33hnvrd30-edmund0.vercel.app" className="font-extrabold text-violet-400"> */}
+                    <Link href="https://www.youtube.com/watch?v=JOEfLlxs9mY" className="font-extrabold text-violet-400"> */}
                       Developed a web application capable of handling thousands of users in the form of a Software-as-a-Service (SaaS)
-                    {/* </Link> */}
+                    </Link>
                   </div>
                 </section>
                 <section className="flex flex-col items-center w-full h-full gap-4">
